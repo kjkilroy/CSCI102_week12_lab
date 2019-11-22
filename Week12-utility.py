b08a01398e34ab979a5ef56933f22b8b62ed5681
kjkilroy <kramerkilroy@mymail.mines.edu>
2019-11-21 19:10:25 -0700

    4

--- Week12-utility.py
+++ Week12-utility.py
@@ -4,18 +4,3 @@
 
 
 
-
-def PrintOutput(a):
-    print('OUTPUT',a)
-
-
-def LoadFile (file_name):
-    file = open(file_name, 'r')
-    text = file.read()
-    text = text.split('\n')
-    return text
-
-def UpdateString(string_1, string_2, n):
-    n = int(n)
-    string_1 = string_1[:n] + string_2 + string_1[n + 1:] 
-    print('OUTPUT', string_1)
